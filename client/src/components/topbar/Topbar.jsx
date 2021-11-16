43a1e995d17df2a58a5034525f5d49b7eaefadf4
i7usen <i7usen@gmail.com>
2021-11-16 17:39:22 +0300

    dash

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -18,7 +18,7 @@ export default function Topbar() {
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Lamasocial</span>
+          <span className="logo">Social-App</span>
         </Link>
       </div>
       <div className="topbarCenter">
